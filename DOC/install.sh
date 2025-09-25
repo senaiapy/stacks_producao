1bd193a910e2ce6da6a2bb5bc5861c6596082830
senaiapy <senaiapy@gmail.com>
2025-09-25 12:47:00 -0300

    feat: update stacks senaia.in yoenvio.de b7g.app

---
+++ DOC/install.sh
@@ -0,0 +1,9 @@
+ 
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install  zip unzip curl git apt-transport-https net-tools libcurl4-openssl-dev nmon htop ufw locales  lsof ecryptfs-utils cryptsetup net-tools nmap zsh nodejs npm ca-certificates  systemd-timesyncd   software-properties-common libxslt1-dev libcurl4 libgeoip-dev  -y
+
+digital ocean mail cineflix.primes@gmail.com
+
+159.65.89.147 n8n-stacks  b7g.app
+
+64.225.61.129	 supabase yoenvio.de
+
